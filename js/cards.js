8c78dea96d130ff8af2ee71d2427d2e3a5b48e67
PaulaZP <mzp1405@gmail.com>
2021-10-03 11:49:54 -0600

    fix: add display function to cards

---
+++ js/cards.js
@@ -0,0 +1,16 @@
+let content = document.querySelectorAll(".js-content");
+let arrows = document.querySelectorAll(".js-arrow");
+
+for (let i = 0; i < arrows.length; i++) {
+    arrows[i].addEventListener("change", () => {
+        for (let i = 0; i < content.length; i++) {
+            if (arrows[i].checked) {
+                content[i].style.display = "block";
+                console.log(content[i], arrows[i])
+            } else {
+                content[i].style.display = "none";
+            }
+        }
+    });
+};
+
